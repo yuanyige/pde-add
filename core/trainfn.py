3ce64f383baa8907a7f62bc78113d0ed6384d866
root <root@kml-task-344894-record-4941890-prod-worker-0.kml-task-344894-record-4941890-prod-worker.ylab.svc.cluster.local>
2023-04-07 15:32:50 +0800

    Fix bug

--- core/trainfn.py
+++ core/trainfn.py
@@ -70,7 +70,7 @@ def train_ladiff_augdiff(dataloader_train, model,
 
         batch_metric["train_loss_nll"] = lossDiff.data.item()
         batch_metric["train_loss_cla"] = lossC.data.item()
-        batch_metric["train_acc"] = (out.data.argmax(dim=1) == y.data).sum().data.item()
+        batch_metric["train_acc"] = (out.data.argmax(dim=1) == y_all.data).sum().data.item()
         batch_metric["scales1"] =  model.scales[0]
         batch_metric["scales2"] =  model.scales[1]
         batch_metric["scales3"] =  model.scales[2]
