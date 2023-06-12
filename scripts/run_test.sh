8bba6c3bb193b9addf521678f2fe41eb0b6cdf95
Yige Yuan <yyg923@126.com>
2023-06-12 16:16:32 +0800

    Update name

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=6 \
 python test.py \
---ckpt_path ./test/resnet-18_pdeadd-none_ntrall_\(Csgdlr0.05cosanlr-Dadamlr0.015\)_e200_b128_aug-augmix-10-10_atk-none \
+--ckpt_path ./save/resnet-18_pdeadd-none_ntrall_\(Csgdlr0.05cosanlr-Dadamlr0.015\)_e200_b128_aug-augmix-10-10_atk-none \
 --load_ckpt model-best-wodiff \
 --main_task ood \
 --type c15
